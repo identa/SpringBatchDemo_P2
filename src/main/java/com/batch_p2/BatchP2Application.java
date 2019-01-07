659d29a8b1d2301affcacb3a5181dca3f8b4efa7
tatrunganh1996 <tatrunganha28@gmail.com>
2019-01-07 15:11:18 +0700

    add csv file and change customer

--- src/main/java/com/batch_p2/BatchP2Application.java
+++ src/main/java/com/batch_p2/BatchP2Application.java
@@ -1,9 +1,11 @@
 package com.batch_p2;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class BatchP2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/batch_p2/model/User.java
+++
@@ -1,4 +0,0 @@
-package com.batch_p2.model;
-
-public class User {
-}
