a4b48e49d843825cd32f922fb5238db9743b73b7
tatrunganh1996 <tatrunganha28@gmail.com>
2019-01-08 15:59:12 +0700

    optimize code

--- src/main/java/com/batch_p2/configuration/Flow2Configuration.java
+++ src/main/java/com/batch_p2/configuration/Flow2Configuration.java
@@ -1,23 +1,16 @@
 package com.batch_p2.configuration;
 
 import com.batch_p2.model.Customer;
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.support.CompositeItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,10 +18,6 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 public class Flow2Configuration {
@@ -76,12 +65,6 @@ public class Flow2Configuration {
         return itemWriter;
     }
 
-//    @Bean
-//    public CompositeItemWriter<Customer> itemWriter() throws Exception{
-//        List<ItemWriter<? super Customer>> writers = new ArrayList<>(2);
-//        writers.add(customerItemWriter());
-//        writers.add(customerItemWriter());
-//    }
     @Bean
     public Step step2() throws Exception {
         return stepBuilderFactory.get("step2")
--- src/main/java/com/batch_p2/configuration/JobConfiguration.java
+++ src/main/java/com/batch_p2/configuration/JobConfiguration.java
@@ -1,33 +1,16 @@
 package com.batch_p2.configuration;
 
-import com.batch_p2.model.Customer;
-import com.batch_p2.processor.PotentialCustomerItemProcessor;
-import com.batch_p2.processor.UpperCaseItemProcessor;
-import com.batch_p2.utils.CustomerFieldSetMapper;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.task.SyncTaskExecutor;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
@@ -41,76 +24,8 @@ public class JobConfiguration {
     @Autowired
     public DataSource dataSource;
 
-//    @Bean
-//    public FlatFileItemReader<Customer> customerItemReader() {
-//        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
-//
-//        reader.setLinesToSkip(1);
-//        reader.setResource(new ClassPathResource("/data/customer.csv"));
-//
-//        DefaultLineMapper<Customer> customerLineMapper = new DefaultLineMapper<>();
-//
-//        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-//        tokenizer.setNames(new String[] {"id", "firstName", "lastName", "birthdate"});
-//
-//        customerLineMapper.setLineTokenizer(tokenizer);
-//        customerLineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
-//        customerLineMapper.afterPropertiesSet();
-//
-//        reader.setLineMapper(customerLineMapper);
-//
-//        return reader;
-//    }
-//
-//    @Bean
-//    public JdbcBatchItemWriter<Customer> customerItemWriter() {
-//        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
-//
-//        itemWriter.setDataSource(this.dataSource);
-//        itemWriter.setSql("INSERT INTO CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)");
-//        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-//        itemWriter.afterPropertiesSet();
-//
-//        return itemWriter;
-//    }
-//
-//    @Bean
-//    public CompositeItemProcessor<Customer, Customer> compositeItemProcessor() throws Exception{
-//        List<ItemProcessor<Customer, Customer>> processorList = new ArrayList<>(2);
-//        processorList.add(new PotentialCustomerItemProcessor());
-//        processorList.add(new UpperCaseItemProcessor());
-//
-//        CompositeItemProcessor<Customer, Customer> processor = new CompositeItemProcessor<>();
-//        processor.setDelegates(processorList);
-//        processor.afterPropertiesSet();
-//
-//        return processor;
-//
-//    }
-//    @Bean
-//    public Step step1() throws Exception{
-//        return stepBuilderFactory.get("step1")
-//                .<Customer, Customer>chunk(10)
-//                .reader(customerItemReader())
-//                .processor(compositeItemProcessor())
-//                .writer(customerItemWriter())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Job job() throws Exception{
-//        return jobBuilderFactory.get("job")
-//                .start(step1())
-//                .build();
-//    }
-
     @Bean
     public Job job(@Qualifier("flow1") Flow flow1, @Qualifier("flow2") Flow flow2){
-//        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("split");
-//
-//        Flow flow = flowBuilder.split(new SyncTaskExecutor())
-//                .add(flow1, flow2)
-//                .end();
 
         return jobBuilderFactory.get("job")
                 .start(flow1)
